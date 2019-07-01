303057a3be656c12dec5cee20e49de69fdb84951
Alan Hurtarte <alan@mypostcard.com>
2019-07-01 11:01:16 -0600

    Added html with data

--- proyecto.R
+++ proyecto.R
@@ -15,15 +15,18 @@ ggcorrplot(cormat, hc.order = TRUE, type = "lower",
 
 lm.fit <- lm(Chance.of.Admit~TOEFL.Score+GRE.Score+CGPA, data = train_data)
 lm2.fit <- lm(Chance.of.Admit~TOEFL.Score+GRE.Score+CGPA+LOR^2+Research^3, data = train_data)
+lm3.fit <- lm(Chance.of.Admit~TOEFL.Score^3+GRE.Score+CGPA+LOR+Research^3, data = train_data)
 summary(lm2.fit)
+summary(lm.fit)
+summary(lm3.fit)
 
-pred_lm <- predict(lm2.fit,test_data)
+plot(lm3.fit)
+
+pred_lm <- predict(lm3.fit,test_data)
 submission <- test_data %>% select(id)
-head(submission)
 submission$`Chance of Admit`<- pred_lm
-head(submission)
 
 df <- data.frame(submission$id, submission$`Chance of Admit`)
 colnames(df) <- c("id", "Chance of Admit")
 head(df)
-write.csv(df, "./Galileo/Econometria/Proyecto/sub2.csv",row.names=FALSE)
+write.csv(df, "./Galileo/Econometria/Proyecto/sub3.csv",row.names=FALSE)
